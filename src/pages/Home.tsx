5ee463a3793888a56b1cbff79fc5974637265186
Dylan Hyrkas <129443532+DylanHyrkas@users.noreply.github.com>
2024-12-10 10:54:28 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HomeButton from "../components/HomeButton"
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import PersonIcon from '@mui/icons-material/Person';
 import { Grid2 as Grid } from "@mui/material";
 
 function Home() {
     return (
         <Grid>
             <HomeButton title="Inventory Management" Icon={DirectionsCarIcon} to="/inventorymanagement"/>
+            <HomeButton title="Account" Icon={PersonIcon} to="/account"/>
         </Grid>
     )
 }
